d622b96dbc00f37b139bc248a4b0e38dff5a2f7c
Juan Camilo Soto Pineda <juancamilo.soto@outlook.com>
2023-10-17 07:41:24 -0500

    Vite Config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/scss/app.scss',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
