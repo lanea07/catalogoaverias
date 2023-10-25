e79553a1166e28e46f395eaa85266a93d71d1d39
Juan Camilo Soto Pineda <jcsoto@flamingo.com.co>
2023-10-25 15:28:54 -0500

    Mailable for Contact form done

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,20 +18,22 @@ class ContactController extends Controller
 
     public function store(Request $request)
     {
-        $validated = $request->validate(
-            [
-                'name'  => 'required',
-                'phone'   => 'required|digits_between:7,10|numeric',
-                'email' => 'required|email:rfc,dns',
-                'notes' => 'sometimes',
-                'id'    => 'required'
-            ]
-        );
-        $product = Product::where('id', '=', $validated['id'])->first();
-        $mailData = ['validated' => $validated, 'product' => $product];
-
-        Mail::to('juan.soto@flamingo.com.co')->locale('es')->queue(new ContactMail($mailData));
-
-        return redirect()->back()->with('status', __('Request succesfuly sent'));
+        try {
+            $validated = $request->validate(
+                [
+                    'name'  => 'required',
+                    'phone'   => 'required|digits_between:7,10|numeric',
+                    'email' => 'required|email:rfc,dns',
+                    'notes' => 'sometimes',
+                    'id'    => 'required'
+                ]
+            );
+            $product = Product::where('id', '=', $validated['id'])->first();
+            $mailData = ['validated' => $validated, 'product' => $product];
+            Mail::to(env('CONTACT_FORM_DESTINATION_ADDRESS', 'juan.soto@flamingo.com.co'))->locale('es')->queue(new ContactMail($mailData));
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('status', __('An error has occurred, please try again later'));
+        }
+        
     }
 }
