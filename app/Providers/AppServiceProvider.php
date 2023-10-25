71600906d34fdf20dc17f2d760805b83a7ce1ad2
Juan Camilo Soto Pineda <juancamilo.soto@outlook.com>
2023-10-25 17:03:36 -0500

    Logging slack configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,6 @@
 namespace App\Providers;
 
 use Exception;
-use Illuminate\Queue\Events\JobFailed;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 
@@ -47,9 +44,5 @@ class AppServiceProvider extends ServiceProvider
         } catch (\Exception $e) {
             throw new Exception('An error has occurred:' . $e);
         }
-
-        Queue::failing(function (JobFailed $event) {
-            Log::alert($event);
-        });
     }
 }
