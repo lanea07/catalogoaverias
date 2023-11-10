fc584d2512534bd0ccac339ef3a11d109bb321f2
Juan Camilo Soto Pineda <juancamilo.soto@outlook.com>
2023-11-10 11:47:23 -0500

    Fixed styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
 </head>
 
-<body class="d-flex flex-column overflow-auto">
+<body class="d-flex flex-column overflow-auto min-vh-100 vh-100">
     <x-bootstrap-default-svgs></x-bootstrap-default-svgs>
     @include('partials.navbar')
     @include('partials.offcanvas')
